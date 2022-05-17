ea61e6320211e6f3e326fc60cf1b19ed4b29db42
MonikaJowal <elmonika11@gmail.com>
2022-05-17 20:27:37 +0530

    updated training_calendar_module

--- training_calendar_service/src/main/java/com/hcl/calendar_managment/web/CalendarController.java
+++ training_calendar_service/src/main/java/com/hcl/calendar_managment/web/CalendarController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -17,8 +18,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.hcl.calendar_managment.domain.Calendar;
+import com.hcl.calendar_managment.domain.Schedule;
 import com.hcl.calendar_managment.service.CalendarService;
 import com.hcl.calendar_managment.serviceimpl.MapValidationErrorService;
 
@@ -32,6 +35,26 @@ public class CalendarController {
 	@Autowired
 	private MapValidationErrorService mapValidationErrorService;
 
+	@Autowired
+	private RestTemplate restTemplate;
+
+	
+	@GetMapping("/findSchedule/{employeeId}")
+	public String fetchScheduleByEmployeeId(@PathVariable("employeeId") Long employeeId) {
+		Schedule schedule = restTemplate.exchange("http://localhost:9095/employees"+"/"+employeeId, HttpMethod.GET,null,Schedule.class).getBody();
+		System.out.println(schedule);
+		return restTemplate.exchange("http://localhost:9095/employees"+"/"+employeeId, HttpMethod.GET,null,String.class).getBody();
+	}
+	
+	@GetMapping("/findAllSchedule")
+	public String fetchAllSchedule() {
+		return restTemplate.exchange("http://localhost:9095/employees/getAll", HttpMethod.GET,null,String.class).getBody();
+	}
+	
+	@GetMapping("/getAllUser")
+	public String fetchAllUser() {
+		return restTemplate.exchange("http://127.0.0.1:8081/user/all", HttpMethod.GET,null,String.class).getBody();
+	}
 	@PostMapping("/create")
 	public ResponseEntity<?> createNewCalendar(@Valid @RequestBody Calendar calendar, BindingResult result) {
 		ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationError(result);
